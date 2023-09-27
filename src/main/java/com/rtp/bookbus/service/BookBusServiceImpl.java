2e50be5396e22907f01ba198160c1d9a24d3196c
Kavya Bansore <kavya.bansore25@gmail.com>
2023-09-27 23:46:28 +0530

    added required annotations in dao and service layer

--- src/main/java/com/rtp/bookbus/service/BookBusServiceImpl.java
+++ src/main/java/com/rtp/bookbus/service/BookBusServiceImpl.java
@@ -3,6 +3,8 @@ package com.rtp.bookbus.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.rtp.bookbus.Dao.BookBusDao;
 import com.rtp.bookbus.entity.Admin;
@@ -11,35 +13,41 @@ import com.rtp.bookbus.entity.Bus;
 import com.rtp.bookbus.entity.Operator;
 import com.rtp.bookbus.entity.User;
 
+@Service
 public class BookBusServiceImpl implements BookBusService {
 
 	@Autowired
 	BookBusDao bookBusDao;
 	
+	@Transactional
 	@Override
 	public List<Admin> ShowAdmins() {
 		
 		return bookBusDao.ShowAdmins();
 	}
 
+	@Transactional
 	@Override
 	public List<User> ShowUsers() {
 		
 		return bookBusDao.ShowUsers();
 	}
 
+	@Transactional
 	@Override
 	public List<Bus> ShowBuses() {
 		
 		return bookBusDao.ShowBuses();
 	}
 
+	@Transactional
 	@Override
 	public List<Operator> ShowOperators() {
 		
 		return bookBusDao.ShowOperators();
 	}
 
+	@Transactional
 	@Override
 	public List<Booking> ShowBookings() {
 		
