91b0793d59544c1089a37ee5a0824e533ff3acd8
Kavya Bansore <kavya.bansore25@gmail.com>
2023-09-27 20:49:59 +0530

    added Admin entity class

---
+++ src/main/java/com/rtp/bookbus/entity/Admin.java
@@ -0,0 +1,63 @@
+package com.rtp.bookbus.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "admins")
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "admin_id")
+    private int adminId;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+    
+    public Admin() {
+    	
+    }
+
+	public int getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "Admin [adminId=" + adminId + ", username=" + username + ", password=" + password + "]";
+	}
+
+	public Admin(String username, String password) {
+		
+		this.username = username;
+		this.password = password;
+	}
+    
+}
